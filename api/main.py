8e02e81f7e49e424dc0e104eadce3393b7420397
grazuc <gonzalorazuc@gmail.com>
2025-05-23 00:33:19 -0300

    tweaks

--- api/main.py
+++ api/main.py
@@ -985,6 +985,7 @@ async def upload_document(
             
             collection_name = sanitize_pg_identifier(f"{userId}_{documentId}")
         
+        os.makedirs(CACHE_DIR, exist_ok=True)  # esto fuerza su existencia en Docker
         current_config = dataclasses.replace(config, collection_name=collection_name, docs_dir=Path(target_dir).parent,cache_dir=Path(CACHE_DIR),reset_vector_collection=True )
         doc_processor = DocumentProcessor(current_config)
 
